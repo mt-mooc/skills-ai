b3a6d0fc29953adceccd5c41460f3c7e42fb072e
Manoj Talagadadeevi <mano854t@gmail.com>
2024-11-10 16:39:18 -0500

    Added import and content in the tailwind.config.js file

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,4 +1,7 @@
+// @ts-check
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./**/*.{html,ts}"],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
