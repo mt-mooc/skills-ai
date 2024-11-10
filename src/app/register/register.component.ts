e376daf3b5afa433cf82e3c3e9e89ccb4f5186ba
Manoj Talagadadeevi <mano854t@gmail.com>
2024-11-10 11:09:08 -0500

    implemented OnInit and created form controls using copilot

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,23 @@
 import { Component } from "@angular/core";
 import { User } from "./../user";
+import { OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
   styleUrls: ["./register.component.css"],
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+    });
+  }
+}
